f9f9f49cbf74fc15b2b7682b9248add5bbcad418
TheNotary <no@email.plz>
2018-02-09 19:01:14 -0600

    adds url decoding for strings coming in from the network

--- src/linux_server_status_display.ino
+++ src/linux_server_status_display.ino
@@ -92,12 +92,14 @@ void processLcdCommands(String param) {
 void getKey(String &dest, String param) {
   int segmentEnd = param.indexOf("=");
   dest = param.substring(0, segmentEnd);
+  dest = urldecode(dest);
 }
 
-// Given "m=message" returns "message"
+// Given "m=message%20now" returns "message now"
 void getVal(String &dest, String param) {
   int segmentBeginning = param.indexOf("=") + 1;
   dest = param.substring(segmentBeginning);
+  dest = urldecode(dest);
 }
 
 // Given params = "a=1&b=2&c=3" call a callback 3 times,
@@ -248,4 +250,47 @@ void printParamData(String param, String key, String val) {
   Serial.println(key);
   Serial.print("    Val:  ");
   Serial.println(val);
+}
+
+// https://github.com/zenmanenergy/ESP8266-Arduino-Examples/blob/master/helloWorld_urlencoded/urlencode.ino
+String urldecode(String str) {
+  String encodedString = "";
+  char c;
+  char code0;
+  char code1;
+  for (int i = 0; i < (int)str.length(); i++) {
+    c = str.charAt(i);
+    if (c == '+'){
+      encodedString+=' ';
+    }
+    else if (c == '%') {
+      i++;
+      code0 = str.charAt(i);
+      i++;
+      code1 = str.charAt(i);
+      c = (h2int_new(code0) << 4) | h2int_new(code1);
+      encodedString += c;
+    }
+    else {
+      encodedString += c;
+    }
+
+    yield();
+  }
+
+ return encodedString;
+}
+
+
+unsigned char h2int_new(char c) {
+  if (c >= '0' && c <='9'){
+    return((unsigned char)c - '0');
+  }
+  if (c >= 'a' && c <='f'){
+    return((unsigned char)c - 'a' + 10);
+  }
+  if (c >= 'A' && c <='F'){
+    return((unsigned char)c - 'A' + 10);
+  }
+  return(0);
 }
